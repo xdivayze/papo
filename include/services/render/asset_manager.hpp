f47dfa590af1c505eccfea7a4f3a58b635420753
ozan <ozangencay123@gmail.com>
2026-05-05 18:17:33 +0200

    added model hpp declarations

--- include/services/render/asset_manager.hpp
+++ include/services/render/asset_manager.hpp
@@ -1,11 +1,10 @@
 #pragma once
 
 #include "collections/robin_hood.hpp"
-#include "mesh.hpp"
+#include "model.hpp"
 #include "services/memory/memory_manager.hpp"
 #include "services/memory/pool_manager.hpp"
 #include "services/memory/stack_allocater.hpp"
-#include "utils/exception.hpp"
 #include <cstddef>
 #include <cstdint>
 #include <string_view>
@@ -16,56 +15,29 @@ class AssetManager {
 public:
   static constexpr const char *TAG = "Asset Manager";
 
-  template <typename T> struct MeshHandle {
+  template <typename T> struct ModelHandle {
     uint32_t id;
-    T *ptr; // pointer to the pool allocated chunk that holds the mesh object
+    T *ptr; // pointer to the pool allocated chunk that holds the model object
 
     bool isValid() const { return id < UINT32_MAX && ptr != nullptr; }
     T *operator->() { return ptr; }
     T &operator*() { return *ptr; }
   };
 
-  template <VertexTypes::VertexLayout TVertex>
-  void loadMeshToGPU(MeshHandle<Mesh<TVertex>> mesh, bool cleanLastCPUData);
+  void loadModelToGPU(ModelHandle<Model> model, bool cleanLastCPUData);
 
-  template <VertexTypes::VertexLayout TVertex>
-  MeshHandle<Mesh<TVertex>> meshFromFilePath(std::string_view filepath);
+  ModelHandle<Model> modelFromFilePath(std::string_view filepath);
 
   AssetManager();
   ~AssetManager();
 
 private:
-  void *meshData_;
-  std::size_t meshDataStackSize_;
+  void *modelData_;
+  std::size_t modelDataStackSize_;
 
   Memory::PoolManager &poolManager_;
   MemoryManager &memoryManager_;
 
-  robin_hood::unordered_flat_map<std::string,
-                                 MeshHandle<VertexTypes::Vertex1P1N1UV>>
-      cache_;
+  robin_hood::unordered_flat_map<std::string, ModelHandle<Model>> cache_;
 };
 } // namespace Service
-
-template <VertexTypes::VertexLayout TVertex>
-Service::AssetManager::MeshHandle<Service::Mesh<TVertex>>
-meshFromFilePath(std::string_view filepath) {
-    
-}
-
-template <VertexTypes::VertexLayout TVertex>
-void Service::AssetManager::loadMeshToGPU(MeshHandle<Mesh<TVertex>> mesh,
-                                          bool cleanLastCPUData) {
-  if (!mesh.isValid())
-    throw Util::PapoException(TAG,
-                              "mesh that is trying to be loaded is not valid");
-
-  mesh->Load();
-  if (cleanLastCPUData) {
-    size_t ptr_last = mesh->indexCount_ * sizeof(unsigned int) +
-                      mesh->vertexCount_ * sizeof(TVertex);
-    memoryManager_.freeStackMemory(mesh.ptr - ptr_last);
-    poolManager_.releaseToPool(mesh.ptr);
-    mesh.ptr = nullptr;
-  }
-}
--- include/services/render/model.hpp
+++ include/services/render/model.hpp
@@ -1,1 +1,49 @@
 #pragma once
+
+#include "glm/ext/matrix_float4x4.hpp"
+#include "services/memory/pool_manager.hpp"
+#include "services/render/mesh.hpp"
+#include <cstddef>
+#include <cstdint>
+
+namespace Service {
+
+struct Material; 
+
+struct MeshInstance {
+  uint32_t meshIndex;
+  uint32_t materialIndex;
+  glm::mat4 transform;
+};
+
+class AssetManager;
+class Model {
+public:
+  friend class AssetManager;
+
+  // load all meshes to the GPU. Release meshes to the pool if clean cpu data.
+  void Load(bool cleanCPUData);
+
+  Model(Memory::PoolManager &poolManager,
+        Mesh<VertexTypes::Vertex1P1N1UV> *meshes, std::size_t meshCount,
+        Material *materials, std::size_t materialCount,
+        MeshInstance *instances, std::size_t instanceCount,
+        bool loaded = false); // fully instantiated model
+
+  ~Model();
+
+private:
+  Memory::PoolManager &poolManager_;
+
+  Mesh<VertexTypes::Vertex1P1N1UV> *meshes_;
+  std::size_t meshCount_;
+
+  Material *materials_;
+  std::size_t materialCount_;
+
+  MeshInstance *instances_;
+  std::size_t instanceCount_;
+
+  bool loaded_;
+};
+} // namespace Service
